a2db884ae2cf1ac635e3e40ae0d08a95191caf3d
JoramM <jorammarkert@gmx.de>
2017-10-27 15:36:20 +0200

    Add detached command and prettify entrypoint

--- README.md
+++ README.md
@@ -2,5 +2,9 @@
 
 ```
 docker build -t minecraft .
-docker run -it --rm -v $PWD/server-data:/minecraft --name minecraft minecraft
+docker run -it --rm -v $PWD/server-data:/minecraft -p 25565:25565 --name minecraft minecraft
+```
+Run the container in detached mode.
+```
+docker run -it -d --rm -v $PWD/server-data:/minecraft -p 25565:25565 --name minecraft minecraft
 ```
--- includes/entrypoint.sh
+++ includes/entrypoint.sh
@@ -2,12 +2,14 @@
 
 if [ -f BuildTools.jar ]
 then
+  echo =========================
   echo starting Minecraft Server
   echo =========================
   java -Xms512M -Xmx1G -XX:+UseConcMarkSweepGC -jar spigot-1.12.2.jar
 else
+  echo =========================
   echo start to build Spigot
-  echo =====================
+  echo =========================
   wget -O BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
   java -jar BuildTools.jar --rev latest
   echo
